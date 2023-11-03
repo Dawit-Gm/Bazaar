5310f3d359676303258c252ddb17bc4d778d572a
Dawit-Gm <4dawitgm@gmail.com>
2023-11-03 17:23:13 +0300

    new5

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 export default function AboutPage() {
   return (
     <Layout>
-      <div className="about text-center ml-4 mr-4">
+      <div className="about text-center ml-10 mr-10">
       <b><h1>  About Us </h1></b>
 <p>Welcome to our online marketplace Bazaar, your one-stop shop for high-quality plastic products. 
 We are a proud business located in the heart of Mercato, the largest open-air market in Africa.
@@ -20,17 +20,17 @@ quality.</p>
 <p>We believe in fair pricing. That’s why we offer our products at manufacturer or distribution level prices for wholesale in addition to retail prices. 
 We want to make sure that you get the best value for your money.</p>
 
-<p><h1>Our Services</h1></p>
+<b><h1>Our Services</h1></b>
 <p>We understand that finding the right product at the right price can be a challenge. 
 That’s why we’re here to help. We take the hassle out of shopping by helping you find the best product 
 and price based on your budget. And if you’re comfortable with it, we can even arrange transportation 
 for your purchases.</p>
 
-<p><h1>Our Location</h1></p>
+<b><h1>Our Location</h1></b>
 <p>Our business is located in Mercato, the largest open-air market in Africa. 
 This strategic location allows us to source the best products and offer them to you at competitive prices.</p>
 
-<p><h1>Come and Explore</h1></p>
+<b><h1>Come and Explore</h1></b>
 <p>We invite you to come and explore our growing list of products. Whether you’re looking for raw materials or
  recycled items, we have something for everyone. And remember, if you have any questions or need help finding 
 a product, our team is always here to help.</p>
