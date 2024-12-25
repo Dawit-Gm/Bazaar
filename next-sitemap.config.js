3530a4af151ff9531bba2c797924ffb0ebf96328
Dawit-Gm <112071712+Dawit-Gm@users.noreply.github.com>
2024-12-25 14:43:57 +0300

    Vercel

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://bazaar.com.et',
+    siteUrl: process.env.SITE_URL || 'https://bazaar-et.vercel.app',
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
