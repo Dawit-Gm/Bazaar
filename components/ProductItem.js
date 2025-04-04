a8c8cf658af5885cc1a80052e7725744706e6b19
Dawit-Gm <112071712+Dawit-Gm@users.noreply.github.com>
2025-04-04 18:56:27 +0300

    sitemap

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -3,7 +3,20 @@ import React from 'react';
 import Image from 'next/image';
 
 
+// Helper function to split name into English and Amharic parts
+const splitBilingualName = (name) => {
+  // Match Latin characters (English) with numbers/symbols/whitespace
+  const englishPart = name.match(/[\p{Script=Latin}\d\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/gu)?.join('')?.trim() || '';
+  
+  // Match Ethiopic characters (Amharic) with numbers/symbols/whitespace
+  const amharicPart = name.match(/[\p{Script=Ethiopic}\d\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/gu)?.join('')?.trim() || '';
+
+  return { englishPart, amharicPart };
+};
+
+
  export default function ProductItem({ product, addToCartHandler }) {
+  const { englishPart, amharicPart } = splitBilingualName(product.name);
 
   return (
     <div className="card">
@@ -17,14 +30,40 @@ import Image from 'next/image';
         sizes="100vw"
         style={{
           width: '100%',
-          height: '75%',
+          height: '73%',
         }}
       ></Image>
       </Link>
       <div className="flex flex-col">
         <Link href={`/product/${product.slug}`}> 
-        <p className="text-md text-left cardname"> 
-         {product.name}</p>
+        <div style={{ 
+          width: '100%',
+          margin: 0,
+        }}>
+          {/* English Name */}
+          {englishPart && (
+            <p style={{ 
+              margin: 0,
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              }}>
+              {englishPart}
+            </p>
+          )}
+          
+          {/* Amharic Name */}
+         {amharicPart && (
+            <p style={{ 
+              margin: 0,
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              }}>
+              {amharicPart}
+            </p>
+          )}
+        </div>
          </Link> 
          <p className="text-left price">
             ETB&nbsp;&nbsp;{product.price}&nbsp;&nbsp; 
@@ -40,7 +79,6 @@ import Image from 'next/image';
     </div>
   );
 }
-
 
 
 
