cc173c2d5f29b16dbac761f759e7d33b531fbd64
Dawit-Gm <112071712+Dawit-Gm@users.noreply.github.com>
2026-09-15 18:35:22 +0300

    google login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,13 +4,45 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import User from '../../../models/User';
 import db from '../../../utils/db';
 
+
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
+import crypto from 'crypto';
+
+
 export default NextAuth({
   session: {
     strategy: 'jwt',
   },
   callbacks: {
-    async jwt({ token, user }) {
-      if (user?._id) token._id = user._id;
+    async jwt({ token, user, account, profile}) {
+        // Social login: create or find user in database
+      if (account && (account.provider === 'google' || account.provider === 'facebook')) {
+        await db.connect();
+        
+        let dbUser = await User.findOne({ email: profile.email });
+
+        if (!dbUser) {
+          // Create new user from OAuth data
+          dbUser = new User({
+            name: profile.name,
+            email: profile.email,
+            password: crypto.randomBytes(32).toString('hex'), // random password for OAuth users
+            isAdmin: false,
+          });
+          await dbUser.save();
+        }
+
+        token._id = dbUser._id.toString();
+        token.isAdmin = dbUser.isAdmin;
+        token.name = dbUser.name;
+        token.email = dbUser.email;
+        
+        await db.disconnect();
+      }
+
+      // For credentials login, user is passed from authorize()
+            if (user?._id) token._id = user._id;
       if (user?.isAdmin) token.isAdmin = user.isAdmin;
       return token;
     },
@@ -39,6 +71,18 @@ export default NextAuth({
         }
         throw new Error('Invalid email or password');
       },
+    }),
+
+    // Google OAuth
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+
+    // Facebook OAuth
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
 });
